ee302a761a7020b76e9df31b7296fa50d1fce2a0
Matt Dies <matthewkdies@gmail.com>
2025-03-10 21:53:57 -0400

    Applying Shellcheck edits to scripts.

--- scripts/build_and_push_football_pool.sh
+++ scripts/build_and_push_football_pool.sh
@@ -5,10 +5,10 @@ set -e
 DATE=$(date +"%d%b%y")
 docker build \
     --file "${COMPOSE_DIR}/football-pool/football-pool/prod/prod.dockerfile" \
-    --tag matthewkdies/football-pool:latest \
-    --tag matthewkdies/football-pool:${DATE} \
-    --build-arg UID=$(id -u football-pool) --build-arg GID=$(id -g football-pool) \
-    ${COMPOSE_DIR}/football-pool/football-pool
+    --tag "matthewkdies/football-pool:latest" \
+    --tag "matthewkdies/football-pool:${DATE}" \
+    --build-arg UID="$(id -u football-pool)" --build-arg GID="$(id -g football-pool)" \
+    "${COMPOSE_DIR}/football-pool/football-pool"
 
-docker push matthewkdies/football-pool:latest
-docker push matthewkdies/football-pool:${DATE}
+docker push "matthewkdies/football-pool:latest"
+docker push "matthewkdies/football-pool:${DATE}"
